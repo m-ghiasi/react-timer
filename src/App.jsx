1de4cff4b0c130083c0fab3ce86fde3d8119bdf5
m-ghiasi <mahdiye71gh@gmail.com>
2024-12-14 13:52:31 +0330

    reactTimer

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import git Timer from './components/Timer'
+
+function App() {
+  
+
+  return (
+   <div>
+    <Timer/>
+    <h1>dgfsjfsdbf</h1>
+
+   </div>
+  )
+}
+
+export default App
