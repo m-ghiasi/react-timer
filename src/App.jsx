416094ea836b06886b3876dd91d324347760c1a1
m-ghiasi <mahdiye71gh@gmail.com>
2024-12-16 12:37:05 +0330

    timer and startStopTimer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import git Timer from './components/Timer'
+import Timer from './components/Timer'
 
 function App() {
   
@@ -7,7 +7,7 @@ function App() {
   return (
    <div>
     <Timer/>
-    <h1>dgfsjfsdbf</h1>
+    
 
    </div>
   )
--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,10 +1,45 @@
+import { useEffect , useState } from "react"
 export default function Time(){
+
+   
+    const [seconds , setSeconds] = useState(0);
+    const [isRunning , setIsRunning] = useState(true)
+
+
+
+    const resetTimer = ()=>{
+        setSeconds(0)
+    }
+
+
+    const startStopTimer = ()=>{
+        setIsRunning(!isRunning)
+    }
+
+
+    useEffect(()=>{
+        let interval;
+
+
+        if(isRunning){
+            interval = setInterval(()=>{
+                setSeconds(prevSeconds => prevSeconds+1);
+            },1000);
+
+        } else {
+            clearInterval(interval);
+        }
+         
+        return ()=> clearInterval(interval)
+    },[isRunning])
+
+
     return(
         <div>
-            <h3>hhhhhhhhhhhhh</h3>
-            <h3>hhhhhhhhhhhhh</h3>
-            <h3>hhhhhhhhhhhhh</h3>
-            <h3>hhhhhhhhhhhhh</h3>
+            <h1>Timer : {seconds}S</h1>
+            <button onClick={startStopTimer}>start Stop Timer</button>
+
+            <button onClick={resetTimer}>Reset</button>
         </div>
     )
 }
